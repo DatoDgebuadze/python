920b6cbd31bc3674e4df2f1c38ab7cd06d002791
DatoDgebuadze <davit.dgebuadze.1@btu.edu.ge>
2021-12-25 15:50:38 +0400

    3

--- codes/1.py
+++ codes/1.py
@@ -1,14 +1,33 @@
-#  Exercise: Accepts Users input date of goal and deadline
-# Print's back:
-# How much time remains until that deadline
+from cryptography.fernet import Fernet
 
-import datetime
-print(datetime.datetime.strptime(deadline, "%d/%m/%Y"))
+paroli = input("Ra aris sheni mtavari paroli? ")
 
-U = input("Sheiyvane mizani da deadline am miznis shesasruleblad\n Aucileblad Gamoyavi : amit\n")
-i_list = U.split(":")
 
-mizani = i_list[0]
-deadline = i_list[1]
+def naxva():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            x, y = data.split("|")
+            print("User:", x, "| Password:", y)
 
-print(i_list)
+
+def damateba():
+    saxeli = input('Momxmareblis Saxeli: ')
+    paroli = input('paroli: ')
+
+    # main mods are w a- creates entirly new one evry turn ,r is read mode, a is append mode most flexible mode it allows to add, create new if needed.
+    with open('passwords.txt', 'a') as f:
+        f.write(saxeli + "\n" + paroli + "\n")
+
+
+while True:
+    archevani = input(
+        "Gsurs Axali Parolis Damateba Tu qsurs arsebulis naxva(naxva, damateba, daachire q rom gamorto: ")
+    if archevani == "q":
+        break
+    if archevani == "naxva":
+        naxva()
+    if archevani == "damateba":
+        damateba()
+    else:
+        print("Shecdoma mogivida")
